b4f2be743b77a223b11e968028917c681921e366
Rishabh Goyal <55811460+rgoyal17@users.noreply.github.com>
2019-11-18 23:20:08 -0800

    Delete test_results_analysis.R

--- test_results_analysis.R
+++
@@ -1,8 +0,0 @@
-library('dplyr')
-test_df <- read.csv("data/final_datasets/test_filtered_2009_present.csv")
-
-summary_info <- test_df %>% 
-  group_by(Certificate.Manufacturer.Name) %>% 
-  summarize(num = n(), emissions_emitted = sum(Rounded.Emission.Result..g.mi., na.rm = TRUE), avg_emission = emissions_emitted/num) %>% 
-  arrange(-avg_emission)
-View(summary_info)

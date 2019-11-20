dcc97c5a0206f4cc1489417f98488262edbded97
xiyH27 <55900329+xiyH27@users.noreply.github.com>
2019-11-20 13:22:23 -0800

    Delete myanalysis.R

--- myanalysis.R
+++
@@ -1,6 +0,0 @@
-install.packages("readxl")
-library("readxl")
-library("dplyr")
-
-vehicle_df <- read.csv("INFO-201-Group-Project/heavy-duty-vehicle-ghg-2015-present.xlsx",
-                       stringsAsFactors = FALSE)

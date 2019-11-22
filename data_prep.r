e03dd1557d576b06635f88ec197d886106e56b9d
Rishabh Goyal <55811460+rgoyal17@users.noreply.github.com>
2019-11-22 15:55:50 -0800

    Update data_prep.r

--- data_prep.r
+++ data_prep.r
@@ -3,48 +3,53 @@ library("xlsx")
 library("dplyr")
 library("readxl")
 
-# original data sets
-vehicles_data <- read.csv(unz("data/original_datasets.zip", "original_datasets/vehicles.csv"))
+# load all original data sets
+vehicles_data <- read.csv(unz("data/original_datasets.zip",
+                              "original_datasets/vehicles.csv"))
 test_results_2014_present <- read_excel("data/original_datasets.zip")
+test_results_2009_2013 <-
+  import("data/light-duty-vehicle-test-results-report-2009-2013.xlsx")
+
 # filter out the columns that we do not need for our analysis
 test_filtered_2014_present <- test_results_2014_present %>%
   select(
-    -`Certified Test Group`, -`Certified Evaporative Family`, -`Vehicle ID`, -`Vehicle Configuration Number`,
-    -`Displacement (L)`, -`Gross Vehicle Weight Rating (lbs.)`, -`Test Drive`, -`Test Drive Description`,
-    -`Transmission Type`, -`Transmission Type Description`, -`Transmission type, if other`, -`Number of Gears`,
-    -`Transmission Lockup Yes/No?`, -`Creeper Gear Yes/No?`, -`Equivalent Test Weight (lbs.)`,
-    -`Vehicle Fuel Category`, -`Vehicle Fuel Category Description`, -`Test Number`, -`Test Procedure`,
-    -`Test Fuel`, -`Certification/In-Use Code`, -`Vehicle Class`, -`Certification Region`,
-    -`Emission Standard Level Code`, -`Upward Diesel Adjustment Factor`, -`Downward Diesel Adjustment Factor`,
-    -`Reactivity Factor (RAF)`
+    -`Certified Test Group`, -`Certified Evaporative Family`, -`Vehicle ID`,
+    -`Vehicle Configuration Number`, -`Displacement (L)`,
+    -`Gross Vehicle Weight Rating (lbs.)`, -`Test Drive`,
+    -`Test Drive Description`, -`Transmission Type`,
+    -`Transmission Type Description`, -`Transmission type, if other`,
+    -`Number of Gears`, -`Transmission Lockup Yes/No?`,
+    -`Creeper Gear Yes/No?`, -`Equivalent Test Weight (lbs.)`,
+    -`Vehicle Fuel Category`, -`Vehicle Fuel Category Description`,
+    -`Test Number`, -`Test Procedure`, -`Test Fuel`,
+    -`Certification/In-Use Code`, -`Vehicle Class`, -`Certification Region`,
+    -`Emission Standard Level Code`, -`Upward Diesel Adjustment Factor`,
+    -`Downward Diesel Adjustment Factor`, -`Reactivity Factor (RAF)`
   )
 
-test_results_2009_2013 <- import("data/light-duty-vehicle-test-results-report-2009-2013.xlsx")
 test_filtered_2009_2013 <- test_results_2009_2013 %>%
   select(
-    -`Certified Test Group`, -`Certified Evaporative Family`, -`Vehicle ID`, -`Vehicle Configuration Number`,
-    -`Displacement (L)`, -`Gross Vehicle Weight Rating (lbs.)`, -`Test Drive`, -`Test Drive Description`,
-    -`Transmission Type`, -`Transmission Type Description`, -`Transmission type, if other`, -`Number of Gears`,
-    -`Transmission Lockup Yes/No?`, -`Creeper Gear Yes/No?`, -`Equivalent Test Weight (lbs.)`,
-    -`Vehicle Fuel Category`, -`Vehicle Fuel Category Description`, -`Test Number`, -`Test Procedure`,
-    -`Test Fuel`, -`Certification/In-Use Code`, -`Vehicle Class`, -`Certification Region`,
-    -`Emission Standard Level Code`, -`Upward Diesel Adjustment Factor`, -`Downward Diesel Adjustment Factor`,
-    -`Reactivity Factor (RAF)`
+    -`Certified Test Group`, -`Certified Evaporative Family`, -`Vehicle ID`,
+    -`Vehicle Configuration Number`, -`Displacement (L)`,
+    -`Gross Vehicle Weight Rating (lbs.)`, -`Test Drive`,
+    -`Test Drive Description`, -`Transmission Type`,
+    -`Transmission Type Description`, -`Transmission type, if other`,
+    -`Number of Gears`, -`Transmission Lockup Yes/No?`,
+    -`Creeper Gear Yes/No?`, -`Equivalent Test Weight (lbs.)`,
+    -`Vehicle Fuel Category`, -`Vehicle Fuel Category Description`,
+    -`Test Number`, -`Test Procedure`, -`Test Fuel`,
+    -`Certification/In-Use Code`, -`Vehicle Class`, -`Certification Region`,
+    -`Emission Standard Level Code`, -`Upward Diesel Adjustment Factor`,
+    -`Downward Diesel Adjustment Factor`, -`Reactivity Factor (RAF)`
   )
 
-test_filtered_2009_present <- rbind(test_filtered_2009_2013, test_filtered_2014_present)
-#View(test_filtered_2009_present)
-#write.csv(test_filtered_2009_present, "data/test_filtered_2009_present.csv", row.names = FALSE)
+# combine two datasets into one, and write it out
+test_filtered_2009_present <- rbind(test_filtered_2009_2013,
+                                    test_filtered_2014_present)
+write.csv(test_filtered_2009_present, "data/test_filtered_2009_present.csv",
+          row.names = FALSE)
 
-# how many cars are represented in the EPA tests.
-test_cars <- test_filtered_2009_present %>%
-  group_by(`Carline Models Covered`) %>%
-  summarise(num = n())
-
-test_years <- test_filtered_2009_present %>%
-  group_by(`Model Year`) %>%
-  summarise(num = n())
-
+# filter and rename columns
 vehicles_filtered <- vehicles_data %>%
   filter(year >= 2009) %>%
   select(
@@ -63,7 +68,8 @@ vehicles_filtered <- vehicles_data %>%
     "Time to charge electric vehicle at 240 V" = charge240,
     "City MGP for main fuel" = city08,
     "City MPG for alternate fuel" = cityA08,
-    "City gas comsumption (gallons/100 miles) in charge depleting mode" = cityCD,
+    "City gas comsumption (gallons/100 miles) in charge depleting mode"
+    = cityCD,
     "City electricity consumption in kw-hrs/100 miles" = cityE,
     "EPA city utility factor for PHEV" = cityUF,
     "Tailpipe CO2 in grams/mile for main fuel" = co2,
@@ -88,7 +94,8 @@ vehicles_filtered <- vehicles_data %>%
     "EPA GHG score on alt fuel" = ghgScoreA,
     "Highway MPG for main fuel" = highway08,
     "Highway MPG for alt fuel" = highwayA08,
-    "highway gasoline consumption (gallons/100miles) in charge depleting mode" = highwayCD,
+    "highway gasoline consumption (gallons/100miles) in charge depleting mode"
+    = highwayCD,
     "highway electricity consumption in kw-hrs/100 miles" = highwayE,
     "EPA highway utility factor (share of electricity) for PHEV" = highwayUF,
     "Vehicle operates on gas / electric blend on low charge" = phevBlended,
@@ -99,14 +106,12 @@ vehicles_filtered <- vehicles_data %>%
     "Cost savings for gas over 5 years comapred to average car" = youSaveSpend,
     "If G or T, this vehicle is subject to gas guzzler tax" = guzzler,
     "Type of alt fuel or advanced tech vehicle" = atvType,
-    "EPA composite gasoline-electricity city MPGe for plug-in hybrid vehicles" = phevCity,
-    "EPA composite gasoline-electricity combined city-highway MPGe for plug-in hybrid vehicles" = phevComb
+    "EPA composite gasoline-electricity city MPGe for plug-in hybrid vehicles"
+    = phevCity,
+    "EPA composite gasoline-electricity combined city-highway MPGe for
+    plug-in hybrid vehicles" = phevComb
   )
 
-vehicle_cars <- vehicles_filtered %>%
-  group_by(model) %>%
-  summarise(num = n())
-
-vehicle_years <- vehicles_filtered %>%
-  group_by(`Model year`) %>%
-  summarise(num = n())
+# write out the dataframe
+write.csv(vehicles_filtered, "data/vehicles_filtered.csv",
+          row.names = FALSE)
